836b8520af9605ba55ba53859921e9adecf34226
Anthony Rodrigues Brito <anthonybrito644@gmail.com>
2026-04-23 14:37:26 -0300

    Update projetojava.sql

--- AulaInterfaceVisual/projetojava.sql
+++ AulaInterfaceVisual/projetojava.sql
@@ -1,12 +1,11 @@
-
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Jun-2023 às 20:44
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.2.0
+-- Tempo de geração: 23/04/2026 às 19:35
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-USE projetojava;
-
 --
 -- Banco de dados: `projetojava`
 --
@@ -27,7 +24,7 @@ USE projetojava;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `alunos`
+-- Estrutura para tabela `alunos`
 --
 
 CREATE TABLE `alunos` (
@@ -37,18 +34,30 @@ CREATE TABLE `alunos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `alunos`
+-- Despejando dados para a tabela `alunos`
 --
 
 INSERT INTO `alunos` (`id`, `nomealuno`, `cursoid`) VALUES
 (1, 'Anthony', 1),
 (2, 'Ana', 7),
-(3, 'Matheus', 1);
+(3, 'Matheus', 1),
+(5, 'Carlos Oliveira', 1),
+(6, 'Mariana Souza', 2),
+(7, 'Ricardo Gomes', 8),
+(8, 'Beatriz Silva', 9),
+(9, 'Jo?o Pedro', 10),
+(10, 'Clara Mendes', 7),
+(11, 'Fernando Vaz', 11),
+(12, 'Julia Costa', 11),
+(13, 'Lucas Rocha', 8),
+(14, 'Amanda Martins', 3),
+(15, 'Bruno Alves', 2),
+(16, 'Sofia Ferreira', 9);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cursos`
+-- Estrutura para tabela `cursos`
 --
 
 CREATE TABLE `cursos` (
@@ -59,46 +68,53 @@ CREATE TABLE `cursos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `cursos`
+-- Despejando dados para a tabela `cursos`
 --
 
 INSERT INTO `cursos` (`id`, `nomecurso`, `nivel`, `duracao`) VALUES
-(1, 'Técnico em Radiologia', 'Técnico', 4),
-(2, 'Tecnico em Informatica', 'Técnico', 3),
-(3, 'Curso de Enfermagem', 'Técnico', 4),
-(7, 'Administração', 'Graduação', 4);
+(1, 'Tecnico em Radiologia', 'Tecnico', 4),
+(2, 'Tecnico em Informatica', 'Tecnico', 3),
+(3, 'Curso de Enfermagem', 'Tecnico', 4),
+(4, 'Sistemas de Informacao', 'Graduacao', 8),
+(5, 'Tecnico em Logistica', 'Tecnico', 3),
+(6, 'Engenharia Civil', 'Graduacao', 10),
+(7, 'Administracao', 'Graduacao', 4),
+(8, 'Engenharia de Software', 'Graduacao', 10),
+(9, 'Medicina', 'Graduacao', 12),
+(10, 'Tecnico em Edificacoes', 'Tecnico', 3),
+(11, 'Ciencia da Computacao', 'Graduacao', 8);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `alunos`
+-- Índices de tabela `alunos`
 --
 ALTER TABLE `alunos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `cursos`
+-- Índices de tabela `cursos`
 --
 ALTER TABLE `cursos`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `alunos`
 --
 ALTER TABLE `alunos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
